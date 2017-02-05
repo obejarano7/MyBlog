421a68e13328686cc0b6fa10b9c4e217ec37c8b0
obejarano7 <oscarcbf5@hotmail.com>
2017-02-05 16:55:28 -0500

    se agregaron las acciones CRUD al controlador de articulos asi como las vistas correspondientes a cada acción

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,27 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(title: params[:article][:title], description: params[:article][:description])
+    if @article.valid?
+      @article.save
+      redirect_to @article
+    else
+      render:new
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 end
